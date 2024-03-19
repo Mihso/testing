54f7f1583d0ab41739c2031f3eaff9f516fe1f6d
Matthew Oshimo <moshimo90696@gmail.com>
2024-03-19 00:48:43 -0700

    added buttons

--- src/item/item.js
+++ src/item/item.js
@@ -1,15 +1,20 @@
 
 import './item.css';
-
+import { useState } from 'react';
 const Item = props => {
 
     const [name, type] = props.children;
+
+    const [counter, updateCounter] = useState(0);
 
     console.log(props);
   return (
     <div className="item">
         <div>{name}</div>
         <div>{type}</div>
+        <div>{counter}</div>
+        <button onClick={()=>{updateCounter(counter + 1)}} >increment</button>
+        <button onClick={()=>{updateCounter(counter - 1)}} >decrement</button>
     </div>
   );
 }
