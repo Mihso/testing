484338d4141c2e7ec35a928d7d5375db4a64bb21
Matthew Oshimo <moshimo90696@gmail.com>
2024-04-22 15:37:41 -0700

    navigatable

--- src/other.js
+++ src/other.js
@@ -6,21 +6,62 @@ import { useState } from 'react';
 function Other() {
 
     const [location, updatelocation]= useState([5,5]);
+    const [pressed, updatePressed] = useState("");
 
     const grid = [];
 
     for(let i = 0; i < 10; i++){
-      grid.push([0,0,0,0,0,0,0,0,0,0]);
+      grid.push([" "," "," "," "," "," "," "," "," "," "]);
     }
 
-    grid[location[0]][location[1]] = 1;
+    grid[location[0]][location[1]] = 'x';
   
+
+  function onPress(press){
+    updatePressed(press.key);
+    let x = location[0];
+    let y = location[1];
+    if(press.key === "s"){
+      if(y == 9){
+        updatelocation([x, 0]);
+      }
+      else{
+        updatelocation([x, y + 1]);
+      }
+    }
+    else if(press.key === "a"){
+      if(x == 0){
+        updatelocation([9, y]);
+      }
+      else{
+        updatelocation([x - 1, y]);
+      }
+    }
+    else if(press.key === "d"){
+      if(x == 9){
+        updatelocation([0, y]);
+      }
+      else{
+        updatelocation([x + 1, y]);
+      }
+    }
+    else if(press.key === "w"){
+      if(y == 0){
+        updatelocation([x, 9]);
+      }
+      else{
+        updatelocation([x, y - 1]);
+      }
+    }
+
+  }
+
   function createRow(row){
     
     return(
       row.map((value, idx) => (
 
-        <div key={idx}>
+        <div className='otherI' key={idx}>
           {value}
         </div>
 
@@ -32,6 +73,7 @@ function Other() {
   return (
     <div className="App">
     <header className="App-header">
+    <input onKeyDown={e=>(onPress(e))}></input>
       <div className='other'>
         {
             grid.map((data, idx) => (
