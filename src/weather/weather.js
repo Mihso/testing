081544c97b8555caf7aa26a7a975476cc174806f
Matthew Oshimo <moshimo90696@gmail.com>
2024-06-03 23:45:33 -0700

    added option to select specific areas forecast

--- src/weather/weather.js
+++ src/weather/weather.js
@@ -6,17 +6,32 @@ function Weather() {
 
     const [title, updateTitle] = useState();
     const [periods, updatePeriods] = useState([]);
-    const [description, updateDescription] = useState();
-    const [url, updateData] = useState();
+    const [areas, updateAreas] = useState([]);
+    const [url, updateURL] = useState("https://api.weather.gov/zones/forecast/ALZ015");
     const [link, updateLink] = useState();
 
     async function getData() {
         const api = "https://api.weather.gov/alerts/active?area=CA";
-        const weather = "https://api.weather.gov/zones/forecast/CAZ006/forecast";
+        const weather = `${url}/forecast`;
+        const areas = "https://api.weather.gov/zones/forecast/";
         const response = await fetch(api);
         const weatherResponse = await fetch(weather);
+        const areasResponse = await fetch(areas);
         const data = await response.json();
         const weatherData = await weatherResponse.json();
+        const areasData  =await areasResponse.json();
+        let listAreas = [];
+
+        let counter = 100;
+
+        for(let l of areasData.features){
+          if(counter == 0){
+          listAreas.push(l.id);
+          counter = 100;
+          }
+          counter-= 1;
+        }
+        updateAreas(listAreas);
         updateTitle(data.title);
         updatePeriods(weatherData.properties.periods);
         // updateData(data.);
@@ -32,7 +47,15 @@ function Weather() {
         <div style={{'fontSize': '40px', 'fontWeight': '400'}}>
             {title} 
         </div>
-
+        <select value={url} onChange={e => updateURL(e.target.value)}>
+        {
+          areas.map((data, idx) => {
+            return(
+              <option value={data} key={idx}>{data}</option>
+            );
+          })
+        }
+      </select>
         <div className="gridder">
         {
             periods.map((data, idx) => {
