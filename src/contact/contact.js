f7ff6ae0c95dd9130ff56da00e274b7bbe3244ab
Matthew Oshimo <moshimo90696@gmail.com>
2024-06-20 01:56:11 -0700

    stylize contact page

--- src/contact/contact.js
+++ src/contact/contact.js
@@ -1,4 +1,4 @@
-import '../App.css';
+import './contact.css';
 import { useState,useRef } from 'react';
 import emailjs from '@emailjs/browser';
 
@@ -24,25 +24,25 @@ function Contact() {
         };
 
   return (
-    <div>
+    <div className='contact-app'>
 
-    <h1>Contact</h1>
+    <h1 className="contact-head">Contact</h1>
 
-    <form ref={form} onSubmit={sendEmail}>
+    <form className="contact-form" ref={form} onSubmit={sendEmail}>
     Name:
     <br></br>
-    <input type="text" name="from_name" />
+    <input className='contact-box' type="text" name="from_name" />
     <br></br>
     E-mail:
     <br></br>
-    <input type="text" name="user_email" />
+    <input className='contact-box' type="text" name="user_email" />
     <br></br>
     Message:
     <br></br>
-    <textarea name="message" rows="10" cols="30" size="50"></textarea>
+    <textarea className='contact-box' name="message" rows="10" cols="30" size="50"></textarea>
     <br></br>
-    <input class="buttoner" type="submit" value="Send" />
-    <input class="buttoner" type="reset" value="Reset" />
+    <input className="buttoner" type="submit" value="Send" />
+    <input className="buttoner" type="reset" value="Reset" />
     </form>
     </div>
   );
