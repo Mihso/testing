652bc10c65ce9b51bda7ba1d098a7367323f77ab
Matthew Oshimo <moshimo90696@gmail.com>
2024-06-19 23:10:01 -0700

    implement emailjs

--- src/contact/contact.js
+++ src/contact/contact.js
@@ -1,41 +1,45 @@
 import '../App.css';
-import { useState } from 'react';
+import { useState,useRef } from 'react';
+import emailjs from '@emailjs/browser';
 
 function Contact() {
 
-    const [title, updateTitle] = useState();
-    const [description, updateDescription] = useState();
-    const [url, updateData] = useState();
-    const [link, updateLink] = useState();
-
-    async function getData() {
-        const api = "https://api.github.com/users/Mihso";
-        const response = await fetch(api);
-        const data = await response.json();
-        updateData(data.avatar_url);
-        updateDescription(data.bio);
-        updateLink(data.html_url);
-    }
-    
-    getData();
+        const form = useRef();
+      
+        const sendEmail = (e) => {
+          e.preventDefault();
+      
+          emailjs
+            .sendForm('service_74chunp', 'template_q9yqntc', form.current, {
+              publicKey: 'DsqhztJWfodwNIwIN',
+            })
+            .then(
+              () => {
+                console.log('SUCCESS!');
+              },
+              (error) => {
+                console.log('FAILED...', error.text);
+              },
+            );
+        };
 
   return (
     <div>
 
     <h1>Contact</h1>
 
-    <form action="mailto:moshimo90696@gmail.com" method="post" enctype="text/plain">
+    <form ref={form} onSubmit={sendEmail}>
     Name:
     <br></br>
-    <input type="text" name="name" />
+    <input type="text" name="from_name" />
     <br></br>
     E-mail:
     <br></br>
-    <input type="text" name="mail" />
+    <input type="text" name="user_email" />
     <br></br>
     Message:
     <br></br>
-    <textarea name="comment" rows="10" cols="30" size="50"></textarea>
+    <textarea name="message" rows="10" cols="30" size="50"></textarea>
     <br></br>
     <input class="buttoner" type="submit" value="Send" />
     <input class="buttoner" type="reset" value="Reset" />
